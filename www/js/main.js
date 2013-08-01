332dd245150a03cd584b448146570c3ceaae4e39
jhonny <steven.alvarez.v@gmail.com>
2013-08-01 15:33:05 -0400

    childbroser para realizar la pasarela de pago, porque antes lo hacia con un window.location = res

--- www/js/main.js
+++ www/js/main.js
@@ -36,9 +36,15 @@ function pagar(){
             //jQuery("#formulario").find("a#redirect_to_paypal").attr("href", res);
             //REDIRECCIONAMOS AL PAYPAL
             //console.log(res);
-            window.location = res;
+            //window.location = res;
+			window.plugins.childBrowser.showWebPage(res, { showLocationBar : false }); // This opens the Twitter authorization / sign in page
+			window.plugins.childBrowser.onLocationChange = function(loc){ locationChange(loc); }; // When the ChildBrowser URL changes we need to track that
         });
     }else{
         alert("Por favor ingrese todos los campos obligatorios!.");
     }
+}
+
+function locationChange(loc){
+    alert(loc);
 }
